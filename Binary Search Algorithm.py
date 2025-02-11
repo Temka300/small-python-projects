2a4b40b95bf3c0436972d7cc4f89d81ad89fb92c
Temka300 <temkaserge@gmail.com>
2025-02-11 07:38:31 -0800

    testing

--- Binary Search Algorithm.py
+++ Binary Search Algorithm.py
@@ -2,71 +2,39 @@
 #Binary Search Algorithm
 #https://www.codementor.io/ilyaas97/6-python-projects-for-beginners-yn3va03fs#binary-search-algorithm
 
-import random
-def createlist():
-    k=list()
-    for i in range(0,10):
-        num = random.randrange(0,101,2)
-        k.append(num)
-    return k
-def check():
-    check=0 #reference to check whether the user choiced a number among the list
-    if user<0:
-        print "Negative numbers are rejected!"
-    elif user>100:
-            print "Remember the numbers range is [0:100]"
-    else:
-        for num in k:
-            if user==num:
-                check=1
-                break
-        if check==1:
-            print "Your number is within the list .."
-        elif check==0:
-            print "Unfortunately, you picked a number out from the list."
-    return check
-def half():
-    ind=0
-    h1=list()
-    h2=list()
-    iterator=iter(k)
-    for i in range(0,5):
-        num=iterator.next()
-        h1.append(num)
-    for i in range(5,10):
-        num=iterator.next()
-        h2.append(num)
-    return h1,h2
-def wh(): #In which half the number is? wh=1:first half,wh=2:second half
-    it=iter(k)
-    if check==1:
-        for num in h1:
-            if user==num:
-                print "Situated in the First half."
-                wh=1
-        for num in h2:
-            if user==num:
-                print "Situated in the Second half."
-                wh=2
-    elif check==0:
-        wh=0
-    return wh
-def apphalf(): #Maintaining just the half of list where situated the choiced number.
-    if wh==1:
-        apphalf=h1
-        print "The New lists content:",apphalf
-    elif wh==2:
-        apphalf=h2
-        print "The New lists content:",apphalf
-    else:
-        apphalf=k
-    return apphalf    
-#MAIN PROGRAM            
-k=createlist()
-print k #Showing the list generated randomly
-user=int(input("Type a number from 0 to 100 : "))
-check=check()
-half()
-h1,h2=half()
-wh=wh() 
-k=apphalf()
+
+def binary_search(sorted_list, search_value):
+    """
+    Perform a binary search on a sorted list to find the index of the search value.
+
+    Args:
+        sorted_list (list): A sorted list of elements.
+        search_value: The value to search for in the list.
+
+    Returns:
+        int: The index of the search value if found, otherwise -1.
+    """
+    left, right = 0, len(sorted_list) - 1  # Define the search boundaries
+
+    while left <= right:
+        mid = (left + right) // 2  # Find the middle index
+
+        if sorted_list[mid] == search_value:  # Check if the middle element is the target
+            return mid  # Return the index of the target
+        if sorted_list[mid] < search_value:  # If the target is greater, search the right half
+            left = mid + 1
+        else:  # If the target is smaller, search the left half
+            right = mid - 1
+
+    return -1  # Return -1 if the target is not found
+
+# Example usage
+SORTED_LIST = [1, 3, 5, 7, 9, 11, 13, 15]
+TARGET = 7
+
+RESULT = binary_search(SORTED_LIST, TARGET)
+
+if RESULT != -1:
+    print(f"Element found at index {RESULT}")
+else:
+    print("Element not found in the array")
